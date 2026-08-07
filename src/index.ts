2585d2a2e36d04b45792e9c67805b437bf0384c6
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-08-07 23:25:12 +0530

    changes

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,21 @@
-console.log("hi")
+import express,{Request ,Response} from 'express';
+import jwt from 'jsonwebtoken'
+
+const app = express ();
+app.use(express.json());
+
+app.post('signup',async(req : Request, res: Response )=>{
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const userExist = users.find ((u)=> username === username )
+
+})
+
+
+app.post('/login',async(req : Request ,res : Response )=>{
+
+})
+
+
+app.listen (3000);
