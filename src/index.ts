423cf43db49b7bd6b4baeeb83b6c4a3cc875c0e0
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-08-08 12:04:03 +0530

    all routes added

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,43 @@ import jwt from 'jsonwebtoken'
 const app = express ();
 app.use(express.json());
 
-app.post('signup',async(req : Request, res: Response )=>{
+app.post('signup',async(req : Request, res: Response )=>{`  `
     const username = req.body.username;
     const password = req.body.password;
 
-    const userExist = users.find ((u)=> username === username )
+})
+
+
+app.post('/signin',async(req : Request ,res : Response )=>{
+
+})
+
+app.post('/orders',async(req : Request, res : Response)=>{
+
+})
+
+app.get('/orders.orderid',async(req : Request, res : Response)=>{
+
+})
+
+app.delete('/orders',async(req : Request, res : Response)=>{
+
+})
+
+app.get('/orders',async(req : Request, res : Response)=>{
+
+})
+
+app.get('/balance/usd',async(req : Request, res : Response)=>{
 
 })
 
 
-app.post('/login',async(req : Request ,res : Response )=>{
+app.get('/balance',async(req : Request, res : Response)=>{
 
 })
+
+
 
 
 app.listen (3000);
