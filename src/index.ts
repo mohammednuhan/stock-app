2c20292c3817a74a5630bafedc6003cf6dc5c382
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-08-08 23:44:00 +0530

    routes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
 import express,{Request ,Response} from 'express';
 import jwt from 'jsonwebtoken'
-
 import { PrismaClient } from "../generated/prisma/client"
 import { PrismaPg } from "@prisma/adapter-pg"
 import bcrypt  from "bcrypt "
 import bun from "bun"
+import authMiddleware from "authMiddleware"
 
 const adapter = new PrismaPg({
     connectionString: process.env.DATABASE_URL!
@@ -45,10 +45,55 @@ app.post('signup',async(req : Request, res: Response )=>{`  `
 
 
 app.post('/signin',async(req : Request ,res : Response )=>{
+        const { username , password } = req.body
 
+        const userExist = await prisma.user.findOne ({
+            where : {
+                username ,
+                password 
+            }
+        })
+        if(userExist){
+            return res.status(403).json({
+                message : "user already logged in"
+            })
+        }
+
+        const hashPassword = await bcrypt.password.comapare (password ,10 )
+
+        const token = jwt.sign ({
+            userId : user.id
+        },process.env.JWT_SECRET
+     )
+
+    res.json ({
+       token : token 
+    })
 })
 
-app.post('/orders',async(req : Request, res : Response)=>{
+
+app.post('/orders',authMiddleware,async(req : Request, res : Response)=>{
+     const userId = req.body
+     const {symbol, side, price, qty, type } = req.body
+
+     const order = await prisma.order.findOne({
+        where : 
+            userId
+     })
+     if(!order){
+        return res.status(403).json({
+            message : "order is sold"
+        })
+     }
+     await prisma.order.create({
+        data : {
+            userId : userId,
+            order : order
+        }
+     })
+     res.json ({
+        message : "order completed"
+     })
 
 })
 
