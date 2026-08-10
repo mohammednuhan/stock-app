e794ecfabba168038b81b440215c72b0d5312988
Mohammed Nuhan <nuhanmohammed04@gmail.com>
2026-08-10 17:18:27 +0530

    restructed

---
+++ backend/dist/authmiddleware.js
@@ -0,0 +1,32 @@
+import jwt from "jsonwebtoken";
+const SECRET_KEY = process.env.JWT_SECRET;
+function authMiddleware(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).json({
+            message: "Authorization token is required",
+        });
+    }
+    else {
+        const token = authHeader.split(" ")[1];
+        if (!token) {
+            return res.status(401).json({
+                message: "Token is missing",
+            });
+        }
+        else {
+            const decoded = jwt.verify(token, SECRET_KEY);
+            console.log(process.env.JWT_SECRET);
+            if (!decoded.userId) {
+                return res.status(403).json({
+                    message: "User ID is missing",
+                });
+            }
+            else {
+                req.userId = decoded.userId;
+                next();
+            }
+        }
+    }
+}
+export default authMiddleware;
---
+++ backend/src/authmiddleware.ts
@@ -0,0 +1,43 @@
+import jwt, { JwtPayload } from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+const SECRET_KEY = process.env.JWT_SECRET as string;
+
+function authMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).json({
+      message: "Authorization token is required",
+    });
+  } else {
+    const token = authHeader.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({
+        message: "Token is missing",
+      });
+    } else {
+      const decoded = jwt.verify(token, SECRET_KEY) as JwtPayload & {
+        userId: string;
+      };
+      console.log(process.env.JWT_SECRET);
+
+      if (!decoded.userId) {
+        return res.status(403).json({
+          message: "User ID is missing",
+        });
+      } else {
+        (req as any).userId = decoded.userId;
+
+        next();
+      }
+    }
+  }
+}
+
+export default authMiddleware;
